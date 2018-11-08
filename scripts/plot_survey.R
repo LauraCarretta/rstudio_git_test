2f2ee2ec8d77ce529c12169dc301819703be16d3
Laura Carretta <laura.carretta91@gmail.com>
2018-11-08 12:06:07 +0100

    boxplots added

--- scripts/plot_survey.R
+++ scripts/plot_survey.R
@@ -4,5 +4,6 @@ surveys_complete <- read_csv("Data/surveys_complete.csv")
 surveys_plot <- ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length))
 surveys_plot + geom_point()
 surveys_plot +  geom_point(alpha = 0.1, aes(color = species_id))
-
+ggplot(data = surveys_complete, mapping = aes(x = species_id, y = weight)) +
+  geom_boxplot()
 
