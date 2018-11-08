9ff9e8297c4137fb6201e7cb42baa991ec227ac8
Juliette Hayer <juliette.hayer@gmail.com>
2018-11-08 11:58:54 +0100

    colored by species

--- scripts/plot_survey.R
+++ scripts/plot_survey.R
@@ -3,4 +3,6 @@ library("tidyverse")
 surveys_complete <- read_csv("Data/surveys_complete.csv")
 surveys_plot <- ggplot(data = surveys_complete, mapping = aes(x = weight, y = hindfoot_length))
 surveys_plot + geom_point()
-surveys_plot +  geom_point(alpha = 0.1, color = "blue")
+surveys_plot +  geom_point(alpha = 0.1, aes(color = species_id))
+
+
